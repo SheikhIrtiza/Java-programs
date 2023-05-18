c090ae9a22e564ee70ac261812af8b322647efd6
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-18 09:55:26 +0530

    Add files via upload

---
+++ ERROR chaning.java
@@ -0,0 +1,18 @@
+public class chain {
+    public void asking() {
+        System.out.println("hearing");
+    }
+
+    public void ask() {
+        System.out.println("here");
+    }
+
+    public void asked() {
+        System.out.println("hered");
+    }
+
+    public static void main(String args[]){
+        chain c = new chain();
+        c.asking().ask().asked();
+    }
+}
