a52d0c688006eb93fb8b17ae96fecdb633fd5388
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-18 09:56:10 +0530

    Add files via upload

---
+++ nonstatic.java
@@ -0,0 +1,14 @@
+ class nonstatic {
+    
+    {  //non static block has only parenthesis and ther is no method over it just curly braces. this block is called at the instantiation of the class
+        System.out.println("non static block"); 
+    }
+     nonstatic()
+     {
+        System.out.println("constructor block");
+     }
+     public static void main(String args[])
+     {
+        nonstatic obj1 = new nonstatic(); //making object
+     }
+}
