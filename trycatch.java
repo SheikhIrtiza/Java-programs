0995b766872322cfb9fcca2a7e65130a358aa793
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-21 22:58:46 +0530

    Update trycatch.java

--- trycatch.java
+++ trycatch.java
@@ -7,7 +7,7 @@ public class trycatch {
         int b = 0;
         int t = 0; 
 
-        try { //applying try method on integer when divided with b(0)
+        try { //applying try method on integer when divided with b(0) 
             t = a / b;
         } catch (Exception e) { //catches the exception and prints the result 
            System.out.println("cannot divide by zero"); 
