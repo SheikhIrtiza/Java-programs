84589f52f4fa685e9175ee1740b053c318f968a8
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-08-17 22:56:49 +0530

    Update superkey.java

--- superkey.java
+++ superkey.java
@@ -4,7 +4,7 @@ class A {
 }
 
 class B extends A //extending the subclass B with parent class A.
-{
+{ 
     int a = 20; //instance variable of sub class.
 
     void show(int a) //method to print the output also has local variable in its parameter.
